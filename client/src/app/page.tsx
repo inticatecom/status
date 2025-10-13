837761db2b224359b31cb441a746efaa8b7fc825
Lucas Stranks <78281961+TwoBrake@users.noreply.github.com>
2025-10-12 18:37:26 -0700

    make dynamic title function properly, fix minor bugs

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,4 @@
+"use server";
 // Resources
 import ky from "ky";
 
@@ -6,58 +7,92 @@ import Image from "next/image";
 import Home from "@/components/Home";
 
 // Definitions
-import type {ServerResponse} from "@/app/api/server/route";
-import type {EndpointResponse} from "@/app/api/endpoints/route";
-import type {Metadata} from "next";
-import type {ResolvingMetadata} from "next/types.js";
-import type {CalloutProps} from "@/lib/definitions";
+import type { ServerResponse } from "@/app/api/server/route";
+import type { EndpointResponse } from "@/app/api/endpoints/route";
+import type { Metadata } from "next";
+import type { ResolvingMetadata } from "next/types.js";
+import type { CalloutProps } from "@/lib/definitions";
 
 /**
  * The root page for the application.
  */
 export default async function RootPage() {
   // Variables
-  const server = await (await ky.get<ServerResponse>("http://localhost:3000/api/server")).json();
-  const endpoints = await (await ky.get<EndpointResponse>("http://localhost:3000/api/endpoints")).json()
-
-  const states = Object.keys(endpoints).map(endpointName => {
-    return endpoints[endpointName].online;
-  });
-
-  let status: CalloutProps["type"];
-
-  if (states.every(state => state)) {
-    status = "success";
-  } else if (states.every(state => !state)) {
-    status = "error";
-  } else {
-    status = "warning";
-  }
+  const server = await (
+    await ky.get<ServerResponse>("http://localhost:3000/api/server")
+  ).json();
+  const endpoints = await (
+    await ky.get<EndpointResponse>("http://localhost:3000/api/endpoints")
+  ).json();
 
   return (
-    <div className={"flex flex-col justify-center items-start max-w-2/5 my-20 mx-auto"}>
+    <div
+      className={
+        "flex flex-col justify-center items-start max-w-2/5 my-20 mx-auto"
+      }>
       <div className={"w-full flex flex-col justify-center items-center"}>
         <Image
           className={"pointer-events-none mb-10"}
           src={"/assets/images/banner.png"}
           alt={"Inticate Logo"}
           unoptimized
-          width={400} height={100}
+          width={400}
+          height={100}
           priority
         />
-        <Home server={server} endpoints={endpoints} status={status}/>
+        <Home
+          server={server}
+          endpoints={endpoints}
+          status={getGlobalStatus(endpoints)}
+        />
       </div>
     </div>
-  )
+  );
 }
 
 /**
  * Set the websites title based on the current status of the services.
  */
-export async function generateMetadata(_: object, parent: ResolvingMetadata): Promise<Metadata> {
-  const templateStr = (await parent).title?.template || ""
+export async function generateMetadata(
+  _: object,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const templateStr = (await parent).title?.template || "";
+  const endpoints = await (
+    await ky.get<EndpointResponse>("http://localhost:3000/api/endpoints")
+  ).json();
+  const status = getGlobalStatus(endpoints);
 
   return {
-    title: templateStr.replace("%s", "Services Online")
+    title: templateStr.replace(
+      "%s",
+      status === "success"
+        ? "Systems Operational"
+        : status === "warning"
+        ? "Partial System Outage"
+        : "System Outage"
+    ),
+  };
+}
+
+/**
+ * Fetches the global status of all services.
+ * @param endpoints The endpoints to check.
+ */
+function getGlobalStatus(endpoints: EndpointResponse): CalloutProps["type"] {
+  const states = Object.keys(endpoints).map((endpointName) => {
+    return endpoints[endpointName].online;
+  });
+
+  let status: CalloutProps["type"];
+
+  if (states.every((state) => state)) {
+    status = "success";
+  } else if (states.every((state) => !state)) {
+    status = "error";
+  } else {
+    status = "warning";
   }
+
+  return status;
 }
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -3,63 +3,83 @@
 import moment from "moment";
 
 // Definitions
-import type {ServerResponse} from "@/app/api/server/route";
-import type {EndpointResponse} from "@/app/api/endpoints/route";
-import type {CalloutProps} from "@/lib/definitions";
+import type { ServerResponse } from "@/app/api/server/route";
+import type { EndpointResponse } from "@/app/api/endpoints/route";
+import type { CalloutProps } from "@/lib/definitions";
 
 // Components
-import {Callout} from "@/components/View";
-import {Status, StatusCategory, StatusEntry} from "@/components/Status";
+import { Callout } from "@/components/View";
+import { Status, StatusCategory, StatusEntry } from "@/components/Status";
 
 /**
  * The client-side version of the home page doing the behind the scene actions like
  * making the requests to the backend server.
  */
-export default function Home({endpoints, server, status}: {
-    endpoints: EndpointResponse,
-    server: ServerResponse,
-    status: CalloutProps["type"]
+export default function Home({
+  endpoints,
+  server,
+  status,
+}: {
+  endpoints: EndpointResponse;
+  server: ServerResponse;
+  status: CalloutProps["type"];
 }) {
-    // Variables
-    const startYear = 2025;
-    const currentYear = new Date().getFullYear();
-    const dateStr = startYear !== currentYear ? `${startYear} - ${currentYear}` : String(startYear);
-    const statusText = {
-        title: status === "success" ? "All Systems Operational" : status === "warning" ? "Partial System Outage" : "All Systems Offline",
-        summary: status === "success" ? "All services are currently determined to be online and accessible. To view the most up-to-date information, try refreshing the page to see if anything has changed." : status === "warning" ? "Some services are currently unavailable. Please read the categories below to identify which services have been affected." : "All services are currently experiencing an outage. Please check our socials to keep up-to-date with the issues."
-    }
+  // Variables
+  const startYear = 2025;
+  const currentYear = new Date().getFullYear();
+  const dateStr =
+    startYear !== currentYear
+      ? `${startYear} - ${currentYear}`
+      : String(startYear);
+  const statusText = {
+    title:
+      status === "success"
+        ? "All Systems Operational"
+        : status === "warning"
+        ? "Partial System Outage"
+        : "All Systems Offline",
+    summary:
+      status === "success"
+        ? "All services are currently determined to be online and accessible. To view the most up-to-date information, try refreshing the page to see if anything has changed."
+        : status === "warning"
+        ? "Some services are currently unavailable. Please read the categories below to identify which services have been affected."
+        : "All services are currently experiencing an outage. Please check our socials to keep up-to-date with the issues.",
+  };
 
-    return (
-        <>
-            <Callout className={"w-full mb-3"} type={status}>
-                <div className={"flex justify-between items-center mb-1"}>
-                    <p className={"font-semibold"}>{statusText.title}</p>
-                    <p className={"text-end"}>Updated {moment(new Date(server.latestPing)).fromNow()}</p>
-                </div>
-                <p className={"text-sm text-pretty"}>Some services are currently unavailable. Please read the categories
-                    below to
-                    identify which services have been affected.</p>
-            </Callout>
-            <Status>
-                <StatusCategory title={"Services"} position={"top"}>
-                    {
-                        endpoints &&
-                        Object.keys(endpoints).map((endpointUrl, index) => (
-                            <StatusEntry key={index} data={endpoints[endpointUrl]}>
-                                {endpointUrl}
-                            </StatusEntry>
-                        ))
-                    }
-                </StatusCategory>
-                <StatusCategory title={"Services"} position={"bottom"}>
-                    {/*<StatusEntry website={"https://lucastranks.com"}>Website</StatusEntry>*/}
-                    {/*<StatusEntry website={"https://lucastranks.com"}>Backend API</StatusEntry>*/}
-                </StatusCategory>
-                <div className={"w-full flex justify-between items-center text-white/70 mt-5"}>
-                    <p>Copyright &copy; {dateStr} | {server.region}</p>
-                    <p>Powered by Inticate</p>
-                </div>
-            </Status>
-        </>
-    );
+  return (
+    <>
+      <Callout className={"w-full mb-3"} type={status}>
+        <div className={"flex justify-between items-center mb-1"}>
+          <p className={"font-semibold"}>{statusText.title}</p>
+          <p className={"text-end"}>
+            Updated {moment(new Date(server.latestPing)).fromNow()}
+          </p>
+        </div>
+        <p className={"text-sm text-pretty"}>{statusText.summary}</p>
+      </Callout>
+      <Status>
+        <StatusCategory title={"Services"} position={"top"}>
+          {endpoints &&
+            Object.keys(endpoints).map((endpointUrl, index) => (
+              <StatusEntry key={index} data={endpoints[endpointUrl]}>
+                {endpointUrl}
+              </StatusEntry>
+            ))}
+        </StatusCategory>
+        <StatusCategory title={"Services"} position={"bottom"}>
+          {/*<StatusEntry website={"https://lucastranks.com"}>Website</StatusEntry>*/}
+          {/*<StatusEntry website={"https://lucastranks.com"}>Backend API</StatusEntry>*/}
+        </StatusCategory>
+        <div
+          className={
+            "w-full flex justify-between items-center text-white/70 mt-5"
+          }>
+          <p>
+            Copyright &copy; {dateStr} | {server.region}
+          </p>
+          <p>Powered by Inticate</p>
+        </div>
+      </Status>
+    </>
+  );
 }
